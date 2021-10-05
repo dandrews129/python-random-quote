ca1a3dd4fdafca3071a793fc8777095aba2c4043
Donovan <donovan.andrews129@gmail.com>
2021-10-05 11:22:16 +0200

    Add random quote selection

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,18 @@
+import random
+
+
 def primary():
-  print("Keep it logically awesome.")
+    #print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+    f = open("quotes.txt")
+    quotes = f.readlines()
+    f.close()
 
-  #print(quotes)
+    last = len(quotes) - 1
+    rnd = random.randint(0, last)
 
-if __name__== "__main__":
-  primary()
+    print(quotes[rnd])
+
+
+if __name__ == "__main__":
+    primary()
